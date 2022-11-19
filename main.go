1b21a9dcb569aa1c0dae0d755e0f859dc12c9ba5
Alexander Vilchez <alexander.vilchez@gdpteam.com>
2022-11-19 15:58:13 -0500

    Updating helloworld 3

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/dummy", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hellow everyone 2!")
+		c.String(http.StatusOK, "Hellow everyone 3!")
 	})
 
 	router.GET("/bye", func(c *gin.Context) {
