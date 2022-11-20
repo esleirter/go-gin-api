33f345f38aa7984496e1f95a50309e2cee93a5b1
Alexander Vilchez <alexander.vilchez@gdpteam.com>
2022-11-20 12:44:39 -0500

    Updating hellow world mesasge to 15

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/dummy", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hellow everyone 14!")
+		c.String(http.StatusOK, "Hellow everyone 15!")
 	})
 
 	router.GET("/bye", func(c *gin.Context) {
