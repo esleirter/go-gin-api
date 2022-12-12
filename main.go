3adc964ea4d367bc448d3f57f54a94d65f878535
Alexander Vilchez <alexander.vilchez@gdpteam.com>
2022-12-12 10:56:39 -0500

    Change hellow message to 24

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/dummy", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hellow everyone 23!")
+		c.String(http.StatusOK, "Hellow everyone 24!")
 	})
 
 	router.GET("/bye", func(c *gin.Context) {
